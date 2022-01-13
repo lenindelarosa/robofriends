7229701e23a5101678f281535d6d8d5f9dabf52e
lenindelarosa <leninmanueld@gmail.com>
2022-01-13 19:58:40 -0400

    robofriends update

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({name, id, email}) => {
     return(
-        <div className='bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src='https://robohash.org/test?200x200'/>
+        <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='robots' src={`https://robohash.org/${id}?200x200'}`}/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>Jane.doe@gmail.com</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
