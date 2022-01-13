68ba6829ef7218eeebae7a18f84aa55554b1e1f5
lenindelarosa <leninmanueld@gmail.com>
2022-01-13 19:34:19 -0400

    adding new files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'tachyons'
+import Card from './Card.js'
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +22,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
