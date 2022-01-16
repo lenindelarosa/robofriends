1fcd1b1f85dd935330cd463d5ebc19a48b770c13
lenindelarosa <leninmanueld@gmail.com>
2022-01-16 17:43:28 -0400

    last build

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../components/CardList.js";
 import SearchBox from "../components/SearchBox.js"
 import './App.css'
 import Scroll from "../components/Scroll.js";
+import ErrorBoundary from "../components/ErrorBoundary.js";
 
 class App extends Component{
     constructor(){
@@ -35,7 +36,9 @@ class App extends Component{
                     <h1 className="f1">Robofriends!</h1>
                     <SearchBox searchChange = {this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots = {filteredRobots}/>
+                        <ErrorBoundary>
+                            <CardList robots = {filteredRobots}/>
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             )
