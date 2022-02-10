6aec187c84971e86d60399181626315a14a317ba
lenindelarosa <leninmanueld@gmail.com>
2022-02-10 11:10:43 -0400

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import 'tachyons';
 import App from './containers/App.js';
 // import reportWebVitals from './reportWebVitals';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
